bab2936749461b375589da7a1d646ddc973f86d6
c-owner <corner3499@gmail.com>
2023-01-31 17:38:38 +0900

    -

--- src/views/posts/PostDetailView.vue
+++ src/views/posts/PostDetailView.vue
@@ -6,16 +6,13 @@ import { ref } from 'vue';
 const route = useRoute();
 const router = useRouter();
 const id = route.params.id;
+/*
+ * ref
+ * 장점 ) 객체 할당 가능
+ * 단점 ) form.value.title, form.value.content 특정 속성 값을 넣을 때 불편함
+ * */
 const form = ref({});
 // let form = reactive({});
-/*
- * 반응형 기초를 배울때 string, number는 ref로 선언
- * 배열이나 객체와 같은 것은 reactive 함수로 선언
- * 그렇지만 form을 ref 객체로 선언했던 이유는
- * ref로 선언하면 장점은 ...data 처럼 한꺼번에 객체 복사를 할 수 있다.
- * reactive는 하나씩 속성과 값을 집어 넣어주어야 한다. (한꺼번에 넣을 경우 일반 객체 값으로 들어가며, 주소값이 달라 value를 사용할 수 없다.
-한꺼번에 넣어 반응형을 상실하게 될 경우, vue Devtools에서 확인해보면 수정할 수 없어진다.
- * */
 
 const fetchPost = () => {
     const data = getPostById(id);
