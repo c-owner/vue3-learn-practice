f44a1b8613f58b34ddad46a15030b4d150448f0d
c-owner <corner3499@gmail.com>
2023-01-31 11:39:08 +0900

    페이지 props 전달

--- src/views/posts/PostDetailView.vue
+++ src/views/posts/PostDetailView.vue
@@ -1,9 +1,31 @@
 <script setup>
 import { useRouter, useRoute } from 'vue-router';
+import { getPostById } from '@/api/posts';
+import { ref } from 'vue';
 
 const route = useRoute();
 const router = useRouter();
 const id = route.params.id;
+const form = ref({});
+// let form = reactive({});
+/*
+ * 반응형 기초를 배울때 string, number는 ref로 선언
+ * 배열이나 객체와 같은 것은 reactive 함수로 선언
+ * 그렇지만 form을 ref 객체로 선언했던 이유는
+ * ref로 선언하면 장점은 ...data 처럼 한꺼번에 객체 복사를 할 수 있다.
+ * reactive는 하나씩 속성과 값을 집어 넣어주어야 한다. (한꺼번에 넣을 경우 일반 객체 값으로 들어가며, 주소값이 달라 value를 사용할 수 없다.
+한꺼번에 넣어 반응형을 상실하게 될 경우, vue Devtools에서 확인해보면 수정할 수 없어진다.
+ * */
+
+const fetchPost = () => {
+    const data = getPostById(id);
+    form.value = { ...data }; // 개체 복사
+    // form = { ...data }; // reactive에서 개체 복사를 하면 반응형을 상실한다.
+    // form.title = data.title;
+    // form.content = data.content;
+    // form.createdAt = data.createdAt;
+};
+fetchPost();
 
 const goListPage = () => {
     router.push({
@@ -23,9 +45,9 @@ const goEditPage = () => {
 
 <template>
     <div>
-        <h2>제목</h2>
-        <p>내용</p>
-        <p class="text-muted">2020-01-01</p>
+        <h2>{{ form.title }}</h2>
+        <p>{{ form.content }}</p>
+        <p class="text-muted">{{ form.createdAt }}</p>
         <hr class="my-4" />
         <div class="row g-2">
             <div class="col-auto">
